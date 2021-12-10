de062f0bd7a74e7c60c7d592f4ab31b63f13d03d
leonikolyan <leonikolyan@hotmail.com>
2021-12-10 23:49:44 +0600

    first commit

---
+++ src/com/aca/Main.java
@@ -0,0 +1,31 @@
+package com.aca;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Task 1
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Input Your Age");
+//        int myAge = scanner.nextInt();
+//        System.out.println("Please enter retirement age");
+//        int retAge = scanner.nextInt();
+//        System.out.println("You have " + (retAge - myAge) + " years left to retire");
+
+        //Task 2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("what is a?");
+//        double a = scanner.nextDouble();
+//        System.out.println("what is b?");
+//        double b = scanner.nextDouble();
+//        if (a < b) System.out.println(a + " < " + b);
+//        if (a > b) System.out.println(a + " > " + b);
+
+        //Tasl 3
+        double a = 15;
+        double b = 2;
+        System.out.println((a + b) / 2);
+    }
+}
